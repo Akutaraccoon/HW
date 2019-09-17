d0a089b1c44181ae9cc269732c003d54943cfba9
Akuta <toniaakuta@gmail.com>
2019-09-17 23:36:44 +0300

    first

---
+++ js.js
@@ -0,0 +1,48 @@
+
+
+// // Задача 1
+// Треба було знайти індекс непарного елемента масиву(забула зкопіювати оригінальну умову)
+
+function filter(el , i){
+  if (el % 2 !== 0){
+    return el;}
+    } ;
+
+
+  
+console.log ([2, 4, 7, 12, 8, 6, 10].findIndex(filter));
+
+
+
+
+
+
+
+
+
+// Задача 5
+
+// function integer(x){
+
+  
+//   let arr = [];
+  
+//   for(let i = 1 ; i < x + 1 ; i++){
+      
+//       arr [ (i - 1) ] = i;
+
+//       return arr;
+  
+
+//   arr.reduce((acc , v)=> Math.pow(v, 3));
+
+//   console.log(arr); 
+      
+//   };
+
+ 
+
+
+    
+// };
+
